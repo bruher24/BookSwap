1159151b62bedc0f7a21c36bde2e886cc914087c
bruher24 <glushkovd2424@gmail.com>
2025-06-24 17:59:53 +0400

    updated filters now there are checkboxes and it looks pretty solid

--- resources/views/utils/filters.blade.php
+++ resources/views/utils/filters.blade.php
@@ -1,46 +1,64 @@
 <!--FILTERS-->
-<div class="btn-group mt-2" role="group" aria-label="Filters">
-    <div class="btn-group" role="group">
-        <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Жанр
-        </button>
-        <ul class="dropdown-menu">
-            @foreach($genres as $genre)
-                <li><a class="dropdown-item" href="#">{{ $genre->name }}</a></li>
-            @endforeach
-        </ul>
-    </div>
+<form>
+    <div class="btn-group mt-2" role="group" aria-label="Filters">
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-outline-dark dropdown-toggle"
+                    data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                Жанр
+            </button>
+            <ul class="dropdown-menu p-0">
+                @foreach($genres as $genre)
+                    <li>
+                        <input type="checkbox" class="btn-check dropdown-item" id="btn-check-genre-{{ $genre->id }}" autocomplete="off">
+                        <label class="btn form-control text-start" for="btn-check-genre-{{ $genre->id }}">{{ $genre->name }}</label>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
 
-    <div class="btn-group" role="group">
-        <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Автор
-        </button>
-        <ul class="dropdown-menu">
-            @foreach($authors as $author)
-                <li><a class="dropdown-item" href="#">{{ $author->formattedName }}</a></li>
-            @endforeach
-        </ul>
-    </div>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-outline-dark dropdown-toggle"
+                    data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                Автор
+            </button>
+            <ul class="dropdown-menu p-0">
+                @foreach($authors as $author)
+                    <li>
+                        <input type="checkbox" class="btn-check dropdown-item" id="btn-check-author-{{ $author->id }}" autocomplete="off">
+                        <label class="btn form-control text-start" for="btn-check-author-{{ $author->id }}">{{ $author->formattedName }}</label>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
 
-    <div class="btn-group" role="group">
-        <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Год издания
-        </button>
-        <ul class="dropdown-menu">
-            @foreach($years as $year)
-                <li><a class="dropdown-item" href="#">{{ $year }}</a></li>
-            @endforeach
-        </ul>
-    </div>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-outline-dark dropdown-toggle"
+                    data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                Год издания
+            </button>
+            <ul class="dropdown-menu p-0">
+                @foreach($years as $year)
+                    <li>
+                        <input type="checkbox" class="btn-check dropdown-item" id="btn-check-year-{{ $year }}" autocomplete="off">
+                        <label class="btn form-control text-start" for="btn-check-year-{{ $year }}">{{ $year }}</label>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
 
-    <div class="btn-group" role="group">
-        <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Тип
-        </button>
-        <ul class="dropdown-menu">
-            @foreach($types as $type)
-                <li><a class="dropdown-item" href="#">{{ $type->name }}</a></li>
-            @endforeach
-        </ul>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-outline-dark dropdown-toggle"
+                    data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                Тип
+            </button>
+            <ul class="dropdown-menu p-0">
+                @foreach($types as $type)
+                    <li>
+                        <input type="checkbox" class="btn-check dropdown-item" id="btn-check-type-{{ $type->id }}" autocomplete="off">
+                        <label class="btn form-control text-start" for="btn-check-type-{{ $type->id }}">{{ $type->name }}</label>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
-</div>
+</form>
