7a32b73d891697c678219b67f45aa9095b3ee060
bruher24 <glushkovd2424@gmail.com>
2025-07-23 18:26:10 +0400

    Added autostart of websockets server

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -6,5 +6,8 @@ php-fpm -D
 # Запуск воркера очереди
 php artisan queue:work redis --queue=job,app,listeners,cache --sleep=3 --tries=3 &
 
+# Запуск сервера вебсокетов
+php artisan reverb:start &
+
 # Держим контейнер активным
 tail -f /dev/null
