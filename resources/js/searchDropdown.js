57cb921c2b3fb3756d9424c71fb5b415b140dd7e
bruher24 <glushkovd2424@gmail.com>
2025-08-06 18:26:51 +0400

    Updated search feature Now it made request by every typed char Added li element leading to all search results

--- resources/js/searchDropdown.js
+++ resources/js/searchDropdown.js
@@ -2,14 +2,14 @@ import * as utils from "./utils.js";
 
 window.axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('api_token')}`;
 
-// TODO: прикрутить реал тайм поиск (по вводу каждого символа)
 let $form = $('#searchForm');
 $form[0].reset();
-$form.on('submit', function (e) {
+let $search = $('#searchInput');
+
+$search.on('input', e => {
     e.preventDefault();
     $('#searchDropdown').remove();
-    let $search = $('#searchInput');
-    searchRequest($search.val())
+    searchRequest(e.target.value)
         .then(response => {
             let $output = $('<ul>', {
                 id: 'searchDropdown',
@@ -44,6 +44,10 @@ $form.on('submit', function (e) {
                 });
                 $output.append('<hr class="searchElement dropdown-divider">');
             }
+            // TODO: добавить страницу / всплывающее окно / другое и отобразить там все результаты поиска
+            $output.append(`<li class="searchElement"><a class="dropdown-item rounded-2 small" href="${1}">Все результаты `
+                + `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">`
+                + `<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg></a></li>`);
             $output.children('hr').last().remove();
             $form.append($output);
         })
