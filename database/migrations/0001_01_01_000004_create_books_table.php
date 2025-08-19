4e14ba9574f5e2888ede305c1d75f95d7f73ca0d
bruher24 <glushkovd2424@gmail.com>
2025-08-19 17:14:34 +0400

    Updated books migration

--- database/migrations/0001_01_01_000004_create_books_table.php
+++ database/migrations/0001_01_01_000004_create_books_table.php
@@ -19,7 +19,7 @@ return new class extends Migration {
             $table->year('publication_year')->nullable();
             $table->string('isbn', 20)->unique()->nullable();
             $table->integer('page_count', false, true);
-            $table->enum('book_type', BookTypeEnum::toArray());
+            $table->foreignId('book_type_id')->constrained('book_types')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('cover_id')->constrained('covers')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
             $table->softDeletes();
