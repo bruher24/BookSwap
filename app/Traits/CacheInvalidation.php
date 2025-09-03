2441d1d64bcc53e50329c24d1dbbe11172598d71
bruher24 <glushkovd2424@gmail.com>
2025-09-03 15:18:09 +0400

    Updated CacheInvalidation trait to clear cached user's chats on every listed action

--- app/Traits/CacheInvalidation.php
+++ app/Traits/CacheInvalidation.php
@@ -12,21 +12,29 @@ trait CacheInvalidation
     {
         static::saved(function ($model) {
             Cache::forget(get_class($model));
+            Log::debug('Forgot: ' . get_class($model));
             if ($model instanceof Chat) {
                 Cache::forget($model->first_user_id . '_chats');
                 Log::debug('Forgot: ' . $model->first_user_id . '_chats');
             }
-            Log::debug('Forgot: ' . get_class($model));
         });
 
         static::deleted(function ($model) {
             Cache::forget(get_class($model));
             Log::debug('Forgot: ' . get_class($model));
+            if ($model instanceof Chat) {
+                Cache::forget($model->first_user_id . '_chats');
+                Log::debug('Forgot: ' . $model->first_user_id . '_chats');
+            }
         });
 
         static::restored(function ($model) {
             Cache::forget(get_class($model));
             Log::debug('Forgot: ' . get_class($model));
+            if ($model instanceof Chat) {
+                Cache::forget($model->first_user_id . '_chats');
+                Log::debug('Forgot: ' . $model->first_user_id . '_chats');
+            }
         });
     }
 }
