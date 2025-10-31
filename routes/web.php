c6c91faaf7e79a48eaf38750d2e0dd13bc01b63f
bruher24 <glushkovd2424@gmail.com>
2025-10-31 09:02:40 +0400

    Configured Xdebug to check test's coverage by phpunit

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('coverage', function () {
+    $html = file_get_contents(__DIR__ . '/../coverage/index.html');
+    if ($html === false) {
+        $html = '';
+    }
+    return response($html, 200)->header('Content-Type', 'text/html');
+});
